c022b2a98980cb9ac4141f22d5926338f8732e11
Weston Kelliher <westonkelliher@gmail.com>
2023-06-24 10:05:51 -0700

    use new controlpads server name; improve rebuild_run.sh to set rustup default and be more verbose

--- rebuild_run.sh
+++ rebuild_run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-set -e
-
-cargo build
-gcc test.c target/debug/libc_controlpads.a -lpthread -ldl
-./a.out
+rustup default nightly
+if cargo build; then
+    echo "rebuild_run.sh: building a.out"
+    gcc test.c target/debug/libc_controlpads.a -lpthread -ldl
+    echo "rebuild_run.sh: running"
+    ./a.out
+else
+    echo "rebuild_run.sh: not building final executable or running it since cargo build failed"
+fi
+rustup default stable
